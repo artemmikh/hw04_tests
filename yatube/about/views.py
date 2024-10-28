7692c267ebf005fe1821e64ffff25d03adb7b528
Михайлов Артём <artem.mikh100@gmail.com>
2024-10-28 15:17:59 +0300

    Удаление раздела "об авторе"

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -1,9 +1,5 @@
 from django.views.generic.base import TemplateView
 
 
-class AboutAuthorView(TemplateView):
-    template_name = 'posts/about/author.html'
-
-
 class AboutTechView(TemplateView):
     template_name = 'posts/about/tech.html'
